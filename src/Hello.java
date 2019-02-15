562602b70862ebad6c9ebea9bddb940822b06ea1
Joseph Scott <jbs198585@gmail.com>
2019-02-14 21:31:19 -0500

    more ints

--- src/Hello.java
+++ src/Hello.java
@@ -7,8 +7,9 @@ public class Hello {
         int myFirstNumber = (10 + 5) + (2 * 10);
         int mySecondNumber = 12;
         int myThirdNumber = myFirstNumber * 2;
+        int myFourthNumber = mySecondNumber * 3;
 
-        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber + myFourthNumber;
 
         int myLastOne = 1000 - myTotal;
 
