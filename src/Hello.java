64f02737351b4799f2791b67394ed20845925f93
Joseph Scott <jbs198585@gmail.com>
2019-02-16 19:18:04 -0500

    take away myfourthnumber

--- src/Hello.java
+++ src/Hello.java
@@ -7,9 +7,8 @@ public class Hello {
         int myFirstNumber = (10 + 5) + (2 * 10);
         int mySecondNumber = 12;
         int myThirdNumber = myFirstNumber * 2;
-        int myFourthNumber = mySecondNumber * 3;
 
-        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber + myFourthNumber;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
 
         int myLastOne = 1000 - myTotal;
 
